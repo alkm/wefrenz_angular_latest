662fe98686bd12d4dc0ea25c92ebd66b09c763c0
Alok <alok_mohanty@gensler.com>
2019-10-25 18:39:49 +0530

    Setup PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'wefrenz_angular_latest',
+    script: './server.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-52-202-7-252.compute-1.amazonaws.com',
+      key: '~/.ssh/wefrenz_keypair.pem',
+      ref: 'origin/master',
+      repo: 'https://github.com/alkm/wefrenz_angular_latest.git',
+      path: '/home/ubuntu/wefrenz_angular_latest',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
