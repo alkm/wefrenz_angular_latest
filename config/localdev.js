00fd459ec9bf594a84c1be159da97ec9d35407ca
Alok <alok_mohanty@gensler.com>
2019-11-09 22:13:16 +0530

    config changed

---
+++ config/localdev.js
@@ -0,0 +1,21 @@
+// production configuration
+// author: Kirk Austin
+module.exports = {
+    appName: 'wefrenz',
+    developerEmails: [
+        'alkmohanty@gmail.com'
+    ],
+    environment:'localdev',
+    server:{
+        port:3000,
+        //hostName:'project.gensler.com'
+        hostName:'localhost' 
+    },
+    socketServer:{
+    	port:3000,
+        hostName:'localhost' 	
+    },
+
+    ssl:{
+    }
+}
---
+++ config/production.js
@@ -0,0 +1,22 @@
+// production configuration
+// author: Kirk Austin
+module.exports = {
+    appName: 'wefrenz',
+    developerEmails: [
+        'alkmohanty@gmail.com'
+    ],
+    environment:'production',
+    server:{
+        port:3000,
+        hostName:'wefrenz.com' 
+    },
+    socketServer:{
+    	port:3000,
+        hostName:'https://www.wefrenz.com' 	
+    },
+    ssl:{
+        certificate:'/etc/letsencrypt/live/wefrenz.com/cert.pem',
+        key:'/etc/letsencrypt/live/wefrenz.com/privkey.pem',
+        port: 8443 
+    }
+}
